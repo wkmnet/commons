e7c4fba1d6f713f2b95e971ee0d4a2262ac17f92
Wukunmeng <wukunmeng@xinpianchang.com>
2017-11-02 20:10:46 +0800

    modify db

--- src/main/java/xin/wukm/commons/config/CommonConfig.java
+++ src/main/java/xin/wukm/commons/config/CommonConfig.java
@@ -116,7 +116,7 @@ public class CommonConfig extends JFinalConfig {
     }
 
     private void initDatabase(Plugins me){
-        String host = "172.30.223.97";
+        String host = "172.30.206.47";
         String port = "3306";
         String database = "commons";
         String userName = "commons";
